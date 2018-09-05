c417a42b27b38c4f5e39c9510f74677fb6927828
namph <phamhuynam.1990@gmail.com>
2018-09-05 16:26:45 +0700

    lay danh sach khachang va hang hoa

--- index.js
+++ index.js
@@ -48,33 +48,124 @@ app.get("/home", function(req, res) {
 });
 
 //get khoan
-app.get("/khoan", function (req, res) {
-  sql.connect( configHD, function(err) {
+app.get("/khoan", function(req, res) {
+  sql.connect(
+    configHD,
+    function(err) {
       if (err) console.log(err);
       var request = new sql.Request();
-      var sQeuery = "select DMKhoanCT.ID,DMKhoanCT.MaKH,DMKhoanCT.SoTan,DMKhoanCT.NguoiSua,DMKhoanCT.NgaySua, DMKH.TEN_KHACH_HANG TenKH, DMKH.DiaChiTat from DMKhoanCT DMKhoanCT inner join DM_Khach_Hang DMKH on DMKhoanCT.MaKH=DMKH.MKH where ID= 55;";
+      var sQeuery =
+        "select DMKhoanCT.ID,DMKhoanCT.MaKH,DMKhoanCT.SoTan,DMKhoanCT.NguoiSua,DMKhoanCT.NgaySua, DMKH.TEN_KHACH_HANG TenKH, DMKH.DiaChiTat from DMKhoanCT DMKhoanCT inner join DM_Khach_Hang DMKH on DMKhoanCT.MaKH=DMKH.MKH where ID= 55;";
       request.query(sQeuery, function(err, recordset) {
         if (err) console.log(err);
         res.send(JSON.stringify(recordset.recordset));
         sql.close();
       });
-    } );
+    }
+  );
 });
 
-app.get("/dondathang", function (req, res) {
-  sql.connect( configHD, function(err) {
+//danh sach khoan
+app.get("/dmkhoan", function(req, res) {
+  sql.connect(
+    configHD,
+    function(err) {
+      if (err) console.log(err);
+      var request = new sql.Request();
+      var sQeuery =
+        "select TOP 7 DMKhoan.ID, DMKhoan.Ten from DMKhoan order by id desc;";
+      request.query(sQeuery, function(err, recordset) {
+        if (err) console.log(err);
+        res.send(JSON.stringify(recordset.recordset));
+        sql.close();
+      });
+    }
+  );
+});
+/**
+ * GET DANH SACH DON DAT HANG
+ */
+
+app.get("/dondathang", function(req, res) {
+  var manvtt = req.param("mnvtt");
+  sql.connect(
+    configHD,
+    function(err) {
       if (err) console.log(err);
       var request = new sql.Request();
 
       request.query(
-        'exec VINA_IAS.dbo.Tien_BaoCaoDSDatHang "namph",1,1;',
+        'exec VINA_IAS.dbo.Tien_BaoCaoDSDatHang "' + manvtt + '",1,1;',
         function(err, recordset) {
           if (err) console.log(err);
           res.send(JSON.stringify(recordset.recordset));
           sql.close();
         }
       );
-    } );
+    }
+  );
+});
+
+/**
+ * GET DANH SACH HANG HOA THEO DAI LY
+ */
+
+app.get("/dshanghoadaily", function(req, res) {
+  var madt = req.param("madt");
+  sql.connect(
+    configHD,
+    function(err) {
+      if (err) console.log(err);
+      var request = new sql.Request();
+
+      request.query(
+        " select DMTP.MATP, DMTP.TENTP, DMTP.TENTP + ' (' + DMBB.TEN_BAO_BI + ')' TenFull " +
+          " from Hoa_Don HD inner join CTHD CTHD on HD.IDKEY=CTHD.IDKEY " +
+          " inner join DMTP DMTP on CTHD.MATP=DMTP.MATP " +
+          " inner join DM_BAO_BI DMBB on DMTP.MBB=DMBB.MBB " +
+          " where HD.Ngay>=dateadd(day,-60,getdate()) " +
+          " and HD.MADT='" +
+          madt +
+          "'" +
+          " group by DMTP.MATP, DMTP.TENTP, DMBB.TEN_BAO_BI order by DMTP.TENTP; ",
+        function(err, recordset) {
+          if (err) console.log(err);
+          res.send(JSON.stringify(recordset.recordset));
+          sql.close();
+        }
+      );
+    }
+  );
+});
+
+/**
+ * GET DANH SACH DAI LY THEO TIEP THI
+ */
+
+app.get("/getdailytheotiepthi", function(req, res) {
+  var manv = req.param("manv");
+  sql.connect(
+    configHD,
+    function(err) {
+      if (err) console.log(err);
+      var request = new sql.Request();
+
+      request.query(
+        " select MKH, TEN_KHACH_HANG, DIA_CHI, MKH + N' - ' + TEN_KHACH_HANG + N' (' + DiaChiTat + N')'  TenKH  " +
+          " from DM_KHACH_HANG where Isnull(KhongDung,0)=0  " +
+          " and (('1' + MKH)='" +
+          manv +
+          "' or (MNVTT in (select * from getdsnhanvien('" +
+          manv +
+          "')))); ",
+        function(err, recordset) {
+          if (err) console.log(err);
+          res.send(JSON.stringify(recordset.recordset));
+          sql.close();
+        }
+      );
+    }
+  );
 });
 
 console.log("Start server at " + new Date().toLocaleString());
@@ -97,7 +188,7 @@ s.on("connection", function(ws, req) {
           var request = new sql.Request();
           var token = md5(msg_json.user + msg_json.pass);
           request.query(
-            "select TEN_NHAN_VIEN, e_mail,'" +
+            "select MNVTT,TEN_NHAN_VIEN, e_mail,'" +
               token +
               "' token,0 Cty, Isnull(IsCaoNhat,0) IsCaoNhat, DIEN_THOAI_1 FROM VINA_IAS.dbo.DM_Nhan_Vien_TT where MNVTT='" +
               msg_json.user +
